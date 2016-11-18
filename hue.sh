67963b9130b05f743ca8d0162c08021bc3d4f0b6
zack matthews <zack@zackmatthews.com>
2016-11-17 23:34:46 -0500

    Add functions to modify CIE xy and saturation val

--- hue.sh
+++ hue.sh
@@ -3,6 +3,7 @@ export HUE_BRIDGE_IP=http://192.168.1.151/
 export BASE_API=http://192.168.1.151/api
 export USER=8LKcg4w717ugIGKYQKyWOkAX9bS0L254d-DPE1Ik
 export CONTENT_JSON="Content-Type: application/json"
+
 create-user() {
 	curl $BASE_API/ -H "Content-Type: application/json" -X POST -d '{"devicetype":"home#${$1}"}' | jq .
 } 
@@ -32,6 +33,16 @@ get-sensors(){
 
 update-sensor(){
 	curl $BASE_API/$USER/sensors/$1 -H CONTENT_JSON -X PUT | jq .
+}
+
+# CIE chart: http://www.developers.meethue.com/sites/default/files/gamut_0.png
+# arg1=light-id, arg2 = CIE x val, arg3 = CIE y val
+
+set-color-xy(){
+	curl $BASE_API/$USER/lights/$1/state -H CONTENT_JSON -X PUT -g -d '{"on":true, "xy":['$2', '$3']}' | jq .
+}
+set-color-sat(){
+	curl $BASE_API/$USER/lights/$1/state -H CONTENT_JSON -X PUT -d '{"on":true, "sat":'$2'}' | jq .
 }
 
 $@
